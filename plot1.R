c8d5b7b2f97104ec4e8779e4271ec9e34a3c3c31
jasonf <jason.foster@orionhealth.com>
2015-11-11 12:48:32 -0700

    Initial checkin

---
+++ plot1.R
@@ -0,0 +1,33 @@
+library(dplyr)
+
+isWeekday <- function(pDate) {
+   !(weekdays(pDate) %in% c('Saturday','Sunday'))
+}
+
+# Read the data
+activity <- read.csv("activity.csv", stringsAsFactors = FALSE)
+
+# Transformations
+# 1. Create a POSIXct date from the character date column
+activity$ctDate <- as.POSIXct(activity$date, format = "%Y-%m-%d")
+
+# Calculate total steps per day, ignoring NA values
+stepsPerDay <- activity %>%
+   group_by(ctDate) %>%
+   summarize(totalSteps = sum(steps, na.rm = TRUE))
+
+# Calculate the mean and median of our total steps to include in the plot
+avgs <- data.frame(as.double(mean(stepsPerDay$totalSteps)), as.double(median(stepsPerDay$totalSteps)))
+
+# Create a histogram for the total steps per day
+hist1 <- ggplot(data=stepsPerDay, aes(stepsPerDay$totalSteps)) +
+   geom_histogram(breaks = seq(0, 25000, by = 5000), col = "black", fill = "cornflowerblue", alpha = .5) +
+   ylim(c(0,30)) +
+   ggtitle("Histogram of Total Steps Per Day") +
+   labs(x = "Total Steps", y = "Count") +
+   geom_vline(aes(xintercept = mean(stepsPerDay$totalSteps, na.rm = TRUE), color = "Mean"), show_guide = TRUE) +
+   geom_vline(aes(xintercept = median(stepsPerDay$totalSteps, na.rm = TRUE), color = "Median"), show_guide = TRUE) +
+   geom_rug(col="darkred",alpha=.1) +
+   scale_colour_manual(name="Legend", values = c(Mean = "firebrick", Median = "forestgreen")) +
+   theme_bw(base_family = "Avenir", base_size = 12)
+print(hist1)
